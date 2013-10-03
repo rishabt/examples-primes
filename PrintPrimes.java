d9b6732b2fdd5153b209fc4bed1a0897ee245932
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-10-03 03:30:03 -0400

    Corrected the indentation and parenthesis in the methods calculateOddPrimes() and printPrimes() - Makes it much easier to read and distinguish

--- PrintPrimes.java
+++ PrintPrimes.java
@@ -43,22 +43,29 @@ public class PrintPrimes {
 
 	      for(int primesFoundSoFar = 1; primesFoundSoFar <= numberOfPrimes; primesFoundSoFar++) {
 	        do {
-	          startPoint = startPoint + 2;
-	          if (startPoint == SQUARE) {
-	            ORD = ORD + 1;
-	            SQUARE = listOfPrimes[ORD] * listOfPrimes[ORD];
-	            MULT[ORD - 1] = startPoint;
-	          }
+	        	startPoint = startPoint + 2;
+	        	
+	        	if (startPoint == SQUARE) {
+		            ORD = ORD + 1;
+		            SQUARE = listOfPrimes[ORD] * listOfPrimes[ORD];
+		            MULT[ORD - 1] = startPoint;
+	        	}
 	          
-	          N = 2;
-	          JPRIME = true;
-	          while (N < ORD && JPRIME) {
-	            while (MULT[N] < startPoint)
-	              MULT[N] = MULT[N] + listOfPrimes[N] + listOfPrimes[N];
-	            if (MULT[N] == startPoint)
-	              JPRIME = false;
-	            N = N + 1;
-	          }
+	        	N = 2;
+	        	JPRIME = true;
+	          
+	        	while (N < ORD && JPRIME) {
+	        		while (MULT[N] < startPoint){
+	        			MULT[N] = MULT[N] + listOfPrimes[N] + listOfPrimes[N];
+	        		}
+	        		
+	        		if (MULT[N] == startPoint){
+	        			JPRIME = false;
+	        		}
+	        		
+	        		N = N + 1;
+	        	}
+	        	
 	        } while (!JPRIME);
 	        
 	        listOfPrimes[primesFoundSoFar] = startPoint;
@@ -69,18 +76,21 @@ public class PrintPrimes {
 	        int PAGENUMBER = 1;
 	        int PAGEOFFSET = 1;
 	        while (PAGEOFFSET <= numberOfPrimes) {
-	          System.out.println("The First " + numberOfPrimes +
+	        	System.out.println("The First " + numberOfPrimes +
 	                               " Prime Numbers --- Page " + PAGENUMBER);
-	          System.out.println("");
-	          for (int ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + numberOfRows; ROWOFFSET++){
-	            for (int C = 0; C < numberOfColumns;C++)
-	              if (ROWOFFSET + C * numberOfRows <= numberOfPrimes)
-	                System.out.format("%10d", listOfPrimes[ROWOFFSET + C * numberOfRows]);
-	            System.out.println("");
-	          }
-	          System.out.println("\f");
-	          PAGENUMBER = PAGENUMBER + 1;
-	          PAGEOFFSET = PAGEOFFSET + numberOfRows * numberOfColumns;
+	        	System.out.println("");
+	        	for (int ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + numberOfRows; ROWOFFSET++){
+		            for (int C = 0; C < numberOfColumns;C++)
+		              if (ROWOFFSET + C * numberOfRows <= numberOfPrimes){
+		                System.out.format("%10d", listOfPrimes[ROWOFFSET + C * numberOfRows]);
+		              }
+		            System.out.println("");
+	        	}
+	        	
+	        	System.out.println("\f");
+	        	PAGENUMBER = PAGENUMBER + 1;
+	        	PAGEOFFSET = PAGEOFFSET + numberOfRows * numberOfColumns;
 	        }
 	    }
+	    
 	}
